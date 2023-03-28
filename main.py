1349ce260b21d3a4c85c67e2cdc3b91be4734bac
02camillecoron <98762053+02camillecoron@users.noreply.github.com>
2023-03-28 23:19:28 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def get_prediction_eos(input_text):
 #CODE IA
 
 #TRADUCTION
-cog_key = 'c6a7ac88371842b08e73ef72e22d335d'
+cog_key = '' #mettre une clé azure
 cog_location = 'francecentral'
 print('Ready to use cognitive services in {} using key {}'.format(cog_location, cog_key))
 
@@ -172,7 +172,7 @@ def godel(question):
         response_GODEL3 = generate3(instruction, knowledge, dialog3)
         #print(f"Answer {i+1}: {response_GODEL3}")
         liste_rep.append(response_GODEL3)
-    #return(liste_rep)
+    
         #Affichage de la traduction des 3 réponses
         translation2 = translate_text(cog_location, cog_key, response_GODEL3, to_lang='fr', from_lang='en')
         print(f"Réponse anglais {i+1}: {response_GODEL3}")        
@@ -259,144 +259,3 @@ async def create(question: Question):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# #Import NWP
-# import os
-# import pandas as pd
-# import numpy as np
-# import transformers
-# import numpy
-# import string
-# import flask
-# import torch
-
-
-
-
-
-# #IMPORT API
-# from typing import Union
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# from fastapi.middleware.cors import CORSMiddleware
-# import json
-
-
-
-
-
-
-# #API ET AUTORISATIONS
-# app = FastAPI()
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-
-
-# #############################   NWP   ###########################################################""
-
-
-# top_k = 5
-# list_proposition=[]
-
-# from transformers.models.camembert.modeling_camembert import CamembertForMaskedLM
-# from transformers.models.camembert.tokenization_camembert import CamembertTokenizer
-
-# tokenizer = CamembertTokenizer.from_pretrained("camembert-base")
-# model = CamembertForMaskedLM.from_pretrained("camembert-base")
-# model.eval()
-
-# def get_prediction_eos(input_text):
-#     try:
-#         input_text += ' <mask>'#mask token for BERT input 
-#         res = get_all_predictions(input_text, top_clean=int(top_k))
-        
-#         return res
-        
-#     except Exception as error:
-#         pass
-
-
-
-# def encode(tokenizer, text_sentence, add_special_tokens=True):
-#     text_sentence = text_sentence.replace('<mask>', tokenizer.mask_token)
-#     if tokenizer.mask_token == text_sentence.split()[-1]:
-#         text_sentence += ' .'## if <mask> is the last token, append a "." so that models dont predict punctuation.  
-#         input_ids = torch.tensor([tokenizer.encode(text_sentence, add_special_tokens=add_special_tokens)])#input ids tensor
-#         mask_idx = torch.where(input_ids == tokenizer.mask_token_id)[1].tolist()[0] #here we are getting ids for masked words
-#     return input_ids, mask_idx
-
-
-# def decode(tokenizer, pred_idx, top_clean):
-#     ignore_tokens = string.punctuation + '[PAD]'#this are the tokens which are to be ignored and not to be used.
-#     tokens = []#empty lsit of tokens
-#     for w in pred_idx:#loop to itrate input index.
-#         token = ''.join(tokenizer.decode(w).split())#this will decode the input id and save it in token variable.
-#         if token not in ignore_tokens:#this loop will check if the word belongs to the token which is to be ignored.
-#             tokens.append(token.replace('##', ''))#this will replace the ## tags if present in the words and will append the words into token list.
-#     return ' '.join(tokens[:top_clean])#this will return top K words in the tokens list
-
-
-
-# def get_all_predictions(text_sentence, top_clean=5):
-#     #test_list = []
-#     input_ids, mask_idx = encode(tokenizer, text_sentence)#we will convert input text to encode form i.e tensors.
-#     # since we will not do backward propogation we will not use gradient calculations.
-#     with torch.no_grad():#https://pytorch.org/docs/stable/generated/torch.no_grad.html 
-#         predict = model(input_ids)[0]#we will pass all the input ids to bert model.
-#     predict_word = decode(tokenizer, predict[0, mask_idx, :].topk(top_k).indices.tolist(), top_clean)#encoded words will now get converted back to words from 0th index to index of mask_ids.and this the magic where the contex will be captured as this is the pretrained BERT model and we will get sensible or meaninfull word at masked_ids place.
-#     predict_tokenize = predict_word.split()
-#     #test_list.append(predict_tokenize)
-#     #print("TEST LISTE : ", test_list)
-#     print(predict_word)
-#     return predict_tokenize
-# # step 5) here we will use above function to encode the input text.
-
-
-
-# def get_prediction_eos(input_text):
-#     try:
-#         input_text += ' <mask>'#masked token is added to input text.
-#         results = get_all_predictions(input_text, top_clean=int(top_k))# here the function will do encoding decoding and produce the results.
-#         return results
-#     except Exception as error:
-#         pass
-
-
-
-
-# #APPEL DE L'API
-# class Question (BaseModel):
-#     question : str
-
-# @app.post('/nwp')
-# async def nwp(question: Question):
-#     question = question.question
-#     print("questions : ", question)
-#     answers_N = get_prediction_eos(str(question))
-#     print("ANSWERS : ", answers_N)
-#     #answers = dialoGPT(str(question))
-#     return answers_N
